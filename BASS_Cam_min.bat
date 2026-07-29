e67e6d6a836bda10b4714014d21fc00394d22d0a
Punkito <blackmetalhans@gmail.com>
2026-07-29 11:59:48 -0400

    Delete BASS_Cam_min.bat

--- BASS_Cam_min.bat
+++
@@ -1,6 +0,0 @@
-@rem Archivo archivado: se movió a archive/BASS_Cam_min.bat
-@echo off
-
-echo Este archivo fue archivado. La versión completa se encuentra en 'archive/BASS_Cam_min.bat'.
-echo Para restaurar, copia el archivo desde 'archive/'.
-exit /b 0
