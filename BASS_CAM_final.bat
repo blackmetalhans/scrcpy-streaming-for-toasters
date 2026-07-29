e6001ab3bbfae732c86e77733f22b2bac5dda25b
Punkito <blackmetalhans@gmail.com>
2026-07-29 11:59:23 -0400

    Delete BASS_CAM_final.bat

--- BASS_CAM_final.bat
+++
@@ -1,6 +0,0 @@
-@rem Archivo archivado: se movió a archive/BASS_CAM_final.bat
-@echo off
-
-echo Este archivo fue archivado. La versión completa se encuentra en 'archive/BASS_CAM_final.bat'.
-echo Para restaurar, copia el archivo desde 'archive/'.
-exit /b 0
